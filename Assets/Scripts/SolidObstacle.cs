89de3a3c4299546fe57f713906f15b204bb01605
Doruk Duman <idduman@hotmail.com>
2022-02-26 21:02:00 +0300

    [Added] prefabs

--- Assets/Scripts/SolidObstacle.cs
+++ Assets/Scripts/SolidObstacle.cs
@@ -14,8 +14,8 @@ namespace ShootRun
 
         public void Damage(Vector3 point)
         {
-            _rb.AddExplosionForce(6f, point, 2f, 0.2f, ForceMode.Impulse);
-            _rb.AddForce(2f * Vector3.forward, ForceMode.Impulse);
+            _rb.AddExplosionForce(3f, point, 2f, 0.1f, ForceMode.Impulse);
+            _rb.AddForce(3f * Vector3.forward, ForceMode.Impulse);
         }
     }
 }
